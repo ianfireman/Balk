55309b0c8e7eb32e5d8c3f7c6b63142ab1fef7ac
imlbf <imlbf@cin.ufpe.br>
2015-11-20 20:20:56 +0000

    User controller e oraganiazcao de pags

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :clientes
   resources :empresas
   resources :home
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   get 'empresa' => 'empresas#index'
   get 'clientes' => 'clientes#index'
   get 'admin' => 'home#admin'
+  get 'signup'  => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
