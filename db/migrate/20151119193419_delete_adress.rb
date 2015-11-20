bf152f3122d1ded949366ca409d6876dd0e0a875
imlbf <imlbf@cin.ufpe.br>
2015-11-20 00:39:54 +0000

    Heroku migrate fix #8

--- db/migrate/20151119193419_delete_adress.rb
+++
@@ -1,8 +0,0 @@
-class DeleteAdress < ActiveRecord::Migration
-  def change
-    remove_column :clientes, :address
-    remove_column :clientes, :cep
-    remove_column :clientes, :city
-    remove_column :clientes, :uf
-  end
-end
