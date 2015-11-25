a4b5d0f9c5d90a8da1c6c3a2667218c2eba38a6b
imlbf <imlbf@cin.ufpe.br>
2015-11-25 08:21:12 +0000

    Permissao para acesso de usuarios definida #18 , Redirecionamento pre login #21 e TDDs #22 #23

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  layout "formL", except: [:show]
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
+  layout "formL", except: [:show, :edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -19,11 +21,42 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Perfil Atualizado"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    # Before filters
+
+    # Confirma um usuario logado
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Você não esta Logado."
+        redirect_to login_url
+      end
+    end
+    
+    # Confirmar se o usuario é o certo
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
     
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # Retorna true se o usuario mandado for o usuario logado
+  def current_user?(user)
+    user == current_user
+  end
+  
   # Retorna o usuario logado, se tiver algum
   def current_user
     if (user_id = session[:user_id])
@@ -41,5 +46,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # Redireciona a um local guardado(ou o padrao)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Quarda a URL que esta tentando ser acessada
+  def store_location
+    session[:forwarding_url] = request.url if request.get? # so se o request for get, pois pode dar erro se for POST, PATCH ou delete
   end
 end
