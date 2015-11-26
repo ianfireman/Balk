b38f03339d6fdce25b6c49e052c318f377aee73f
imlbf <imlbf@cin.ufpe.br>
2015-11-26 23:40:48 +0000

    Helper to right url

--- app/helpers/account_activations_helper.rb
+++ app/helpers/account_activations_helper.rb
@@ -1,2 +1,8 @@
 module AccountActivationsHelper
+  
+  def right_url
+    urlStr = edit_account_activation_url(@user.activation_token, email: @user.email)
+    urlStr = urlStr[24..-1]
+    urlStr = "https://floating-plains-2613" + urlStr
+  end
 end
