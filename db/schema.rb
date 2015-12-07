0b6313350e391ce6632ade1e394dc79ae77ed573
imlbf <imlbf@cin.ufpe.br>
2015-12-07 02:11:29 +0000

    Fix all layout

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20151205193146) do
     t.integer  "empresa_id"
   end
 
+  add_index "clientes", ["created_at"], name: "index_clientes_on_user_id_and_empresa_id_and_created_at"
   add_index "clientes", ["empresa_id", "created_at"], name: "index_clientes_on_empresa_id_and_created_at"
   add_index "clientes", ["empresa_id"], name: "index_clientes_on_empresa_id"
 
