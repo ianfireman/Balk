961c9a62238ed6c07796617a29a901dd801ad07d
dmop <dmop@cin.ufpe.br>
2015-12-13 02:12:08 +0000

    BDD

--- features/step_definitions/trocaempresa_steps.rb
+++ features/step_definitions/trocaempresa_steps.rb
@@ -1,15 +1,33 @@
   Given(/^I am logged on Nobalk$/) do
-  visit'https://fodasse-dmop.c9users.io/admin'
-  end
+     
+     visit'https://bigboss-dmop.c9users.io/login'
+    @user = User.create!(
+             :name => "danilo",
+             :email =>"danilo@danilo.com",
+             :password => "danilo"	,	 
+             
+)
+@user.activated = true
+
+@user.save!
+
+    fill_in 'username', :with => 'danilo@danilo.com'  
+
+    fill_in 'password', :with => 'danilo'  
+
+      click_button("Entrar")
+  visit'https://bigboss-dmop.c9users.io/admin'
+      #assert_current_path('https://nobalk-imlbf.c9.io/pedidos')
+    end
   
   Then(/^I click in the button$/) do
-    visit"https://fodasse-dmop.c9users.io/empresas"
-    end
+  click_link("Nike")
+  end
   And(/^I choose another enterprise$/) do
-    visit"https://fodasse-dmop.c9users.io/empresas"
+    visit"http://nobalk-imlbf.c9.io/active?empresaN=1"
     end
   Then(/^I should see the switch of enterprises$/) do
-  visit"https://fodasse-dmop.c9users.io/admin"
+  visit"http://nobalk-imlbf.c9.io/empresas"
   end
 
 
